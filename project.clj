4386ec9bf3ee86192b1ad4370b76e2fe2a9ddcf4
igorssilva <igorsilvaspecial@gmail.com>
2018-06-06 19:02:01 -0300

    teste commit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lp "0.1.0"
   :description "Trabalho de LP"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/igorssilva/trablp-clojure"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot lp.core
   :target-path "target/%s"
