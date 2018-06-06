bab380931a00c6d715931c3500ae9d7eb0d1ee94
igorssilva <40007423+igorssilva@users.noreply.github.com>
2018-06-06 18:46:53 -0300

    Add files via upload

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lp/core_test.clj
@@ -0,0 +1,7 @@
+(ns lp.core-test
+  (:require [clojure.test :refer :all]
+            [lp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
